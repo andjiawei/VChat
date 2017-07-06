c88a2df3dfbae56d538f90c406b28071af31cb2a
jiawei <jiawei@mosainet.com>
2017-07-06 23:14:35 +0800

    增加欢迎页

---
+++ app/src/main/java/com/jiawei/vchat/WelcomeActivity.java
@@ -0,0 +1,21 @@
+package com.jiawei.vchat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        findViewById(android.R.id.content).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(WelcomeActivity.this,MainActivity.class));
+                finish();
+            }
+        },3000);
+    }
+}
